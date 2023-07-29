f2cfed353573c3183fd46368e340a4f1aece99d4
Ahmed <109826282+A7medEG@users.noreply.github.com>
2023-07-29 14:37:57 +0300

    🎵

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "13251350"))
+API_HASH = os.getenv("API_HASH", "66c0eacb36f9979ae6d153f207565cd6")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "token bot here")
+SUDO_ID = int(os.getenv("SUDO_ID", "13251350"))
