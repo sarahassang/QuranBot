c45b19e829b91f24d0e296dba16a0bc5c8fe1126
sarahassang <145998215+sarahassang@users.noreply.github.com>
2024-02-01 20:06:24 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 API_ID = int(os.getenv("API_ID", "13251350"))
 API_HASH = os.getenv("API_HASH", "66c0eacb36f9979ae6d153f207565cd6")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "token bot here")
-SUDO_ID = int(os.getenv("SUDO_ID", "13251350"))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6847278773:AAGLeagI9-Bbzd-VjqZZRuqgl1vJ0MudP9I")
+SUDO_ID = int(os.getenv("SUDO_ID", "6709333755"))
